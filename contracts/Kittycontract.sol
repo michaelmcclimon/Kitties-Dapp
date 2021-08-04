fa3ef2997c217a25ae8f2003a858d074a72a4e4d
Michael McClimon <82418591+michaelmcclimon@users.noreply.github.com>
2021-08-04 19:13:08 -0400

    update

--- contracts/Kittycontract.sol
+++ contracts/Kittycontract.sol
@@ -2,27 +2,57 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "./IERC721.sol";
 
-abstract contract Kittycontract is IERC721 {
-
-    uint256 private _totalSupply;
-
+ contract Kittycontract is IERC721 {
+   
     // Token name
-    string private _name;
+    string public constant name = "MickeyKitties";
 
     // Token symbol
-    string private _symbol;
+    string public constant symbol = "GG";
 
-    constructor(string memory name_, string memory symbol_) {
+    event Birth(address owner, uint256 kittenId, uint256 momId, uint256 dadId, uint256 genes);
 
-        name_ = "Gigi";
-        symbol_ = "GG";
-        _name = name_;
-        _symbol = symbol_;
-        }
+
+    struct Kitty {
+        uint256 genes;
+        uint64 birthTime;
+        uint32 momId;
+        uint32 dadId;
+        uint16 generation;
+    }
+
+    Kitty[] kittes;
 
     mapping (uint256 => address) public kittyIndexToOwner; // tokenId => kitty owner
     mapping (address => uint) ownershipTokenCount; //count of how many kitties each owner has
-    mapping (uint256 => address) kittyIndexToApproved;
+    
+    function createKittyGen0(uint256 _genes) public onlyOwner {
+
+    }
+
+    function _createKitty(
+        uint256 _momId,
+        uint256 _dadId,
+        uint256 _generation,
+        uint256 _genes,
+        address _owner
+    )  private returns (uint256) {
+        Kitty memory _kitty = Kitty({
+            genes: _gene,
+            birthTime: uint64(now),
+            momId: uint32(_momId),
+            dadId: uint32(_dadId),
+            generation: uint16(_generation)
+        });
+
+        uint256 newKittenId = kitties.push(_kitty) - 1;
+
+        emit Birth(_owner, newKittenId, _momId, _dadId, _genes);
+
+        _transfer(address(0), _owner, newKittenId);
+
+        return newKittenId;
+    }
 
      //Returns the number of tokens in ``owner``'s account.
     function balanceOf(address owner) external view override returns (uint256) {
@@ -38,13 +68,13 @@ abstract contract Kittycontract is IERC721 {
     /*
      * @dev Returns the name of the token.
      */
-    function name() external view override returns (string memory){
+    function catName() external view override returns (string memory){
         return _name;
     }
     /*
      * @dev Returns the symbol of the token.
      */
-    function symbol() external view override returns (string memory) {
+    function catSymbol() external view override returns (string memory) {
         return _symbol;
     }
     /**
@@ -59,23 +89,26 @@ abstract contract Kittycontract is IERC721 {
         return kittyIndexToOwner[tokenId];
     }
 
-    function transfer(address to, uint256 tokenId) external override {
+    function transfer(address _to,uint256 _toeknId) external {
 
-        //check that 'to' address is not contract address or zero address
-        require(to != address(0) && to != address(this));
+        require(_to != address(0));
+        require(_to != address(this));
+        require(_owns(msg.sender, _to, _tokenId));
+    }
 
-        //requiring tokenId is owned by msg.sender
-        require(kittyIndexToOwner[tokenId] == msg.sender);
 
-        //updating counts
-        ownershipTokenCount[msg.sender]--;
-        ownershipTokenCount[to]++;
+    function _transfer(address _from, address _to, uint256 tokenId) internal {
+        ownershipTokenCount[_to]++;
 
-        //making transfer to 'to'
-        kittyIndexToOwner[tokenId] = to;
+        kittyIndexToOwner[_tokenId] = _to;
 
-        //emit event
-        emit Transfer(msg.sender,to,tokenId);
+        if (_from != address(0)) {
+            ownershipTokenCount[_from]--;
+        }
+
+        // Emit the transfer event
+        emit Transfer(_from, _to, _tokenId);
+
     }
 
  }   
